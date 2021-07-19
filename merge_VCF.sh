8537900874342aa075b1a83a4318ab61722c4602
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2021-07-19 14:14:40 +0930

    Create merge_VCF.sh

---
+++ merge_VCF.sh
@@ -0,0 +1,8 @@
+module purge
+module load arch/arch/haswell
+module load arch/haswell
+module load modulefiles/arch/haswell
+
+module load vcftools/0.1.12a-GCC-5.3.0-binutils-2.25
+
+vcf-merge <input1>.vcf <input2>.vcf > <merged>.vcf
